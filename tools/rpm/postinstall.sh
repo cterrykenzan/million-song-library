07134ac0ba6c4a55c3b4a0f5fa45817b8201fa1b
Chris Terry <cterry@kenzan.com>
2017-01-24 15:26:10 -0700

    Remove excess crap from trying to configure environment using build process

--- tools/rpm/postinstall.sh
+++ tools/rpm/postinstall.sh
@@ -7,8 +7,6 @@ export
 # when this script runs, Jenkins is out of the picture and can't pass in vars
 INSTALLING_SERVICE=EDITME
 echo "Service to install: $INSTALLING_SERVICE"
-INSTALLING_CASSANDRA=EDITCASSANDRA
-echo "Cassandra Host is at: $INSTALLING_CASSANDRA"
 
 if [ $INSTALLING_SERVICE == EDITME ]; then
   echo "Script was supposed to be edited by jenkins job first."
@@ -17,28 +15,6 @@ fi
 
 chmod a+rwx /var/log/msl
 JAR_TO_EXEC=`find /opt/kenzan/$INSTALLING_SERVICE-*-with-dependencies.jar`
-#echo "Found a jar at $JAR_TO_EXEC"
-
-#Commented out because environment config is now moved out of the build phase.
-#Should be safe to delete.
-#cd /tmp/
-#if unzip $JAR_TO_EXEC config.properties; then
-  # #chmod a+rw config.properties
-  # #sed -i -e s/127.0.0.1/$INSTALLING_CASSANDRA/ config.properties
-  # rm -rf config.properties
-  # echo "192.168.4.93 cassandra-msl.kenzan-devops.com" >> /etc/hosts
-  # echo "creating config"
-  # echo "domain=$INSTALLING_CASSANDRA" > config.properties
-  # echo "keyspace=msl" >> config.properties
-  # echo "region=us-east-1" >> config.properties
-  # cat config.properties
-  # echo "adding to zip"
-#  zip -f $JAR_TO_EXEC config.properties
-#else
-#  echo "Editing config failed during jar unpack, reason:"
-#  unzip $JAR_TO_EXEC config.properties#
-#  exit 1
-#fi
 
 if [ `echo $JAR_TO_EXEC | wc -l` == 1 ]; then
   echo "Found $JAR_TO_EXEC"

ae87aa0741c40da2959cb603d919575f9ba75023
Chris Terry <cterry@kenzan.com>
2017-01-24 15:15:48 -0700

    Jenkins support scripts added, RPM packaging no longer doing env config

---
+++ tools/jenkins/build.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+echo $BUILD_NUMBER > $WORKSPACE/build_number
+cd $WORKSPACE;
+
+CASSANDRA_HOST="cassandra-msl.$BASE_DOMAIN"
+
+echo "Cassandra is at $CASSANDRA_HOST"
+
+for file in `grep -Rl "local=127.0.0.1" *`; do
+  sed -i -e s/local=127.0.0.1/domain=$CASSANDRA_HOST/g -e 's/us-west-2/$AWS_REGION/g' $file;
+done
+for file in `grep -Rl "domain=127.0.0.1" *`; do
+  sed -i -e s/domain=127.0.0.1/domain=$CASSANDRA_HOST/g -e 's/us-west-2/$AWS_REGION/g' $file;
+done
+echo "Setting default cluster to the msl monolith elb"
+for file in `grep -Rl "127.0.0.1" *`; do
+  sed -i -e "s/127.0.0.1/$CASSANDRA_HOST/g" -e 's/us-west-2/$AWS_REGION/g' $file;
+done
+
+#Service .poms expect to package RPMs with individualized installer names
+# Should generalize when time allows.
+#RPM plugin currently grabs script from the service working dir
+# Can get the two-for-one deal by referencing the install script directly once it's refactored
+echo "Configuring Installation Script"
+INSTALLER_FILENAME=`echo $SERVICE_NAME | sed -e s/msl/install/`
+cp $WORKSPACE/tools/rpm/postinstall.sh $WORKSPACE/server/$SERVICE_NAME/$INSTALLER_FILENAME.sh
+sed -i -e s/=EDITME/=$SERVICE_NAME/ -e s/=EDITCASSANDRA/=$CASSANDRA_HOST/ $WORKSPACE/server/$SERVICE_NAME/$INSTALLER_FILENAME.sh
+cat $WORKSPACE/server/$SERVICE_NAME/$INSTALLER_FILENAME.sh
+
+echo "Setting default cluster to the msl monolith elb"
+for file in `grep -Rl "127.0.0.1" *`; do
+  sed -i -e "s/127.0.0.1/$CASSANDRA_HOST/g" -e 's/us-west-2/$AWS_REGION/g' $file;
+done
+
+#Installing all msl requirements so this script can remain generalized.
+cd $WORKSPACE/msl-pages;
+npm install -y
+bower install
+npm run generate-swagger-html
+npm install webpack
+npm install -y protractor
+npm install -y selenium-webdriver
+
+#The extra build steps seem superfluous but work consistently.
+#Same as above - refine when time allows.
+cd $WORKSPACE/msl-pages
+npm run build-server
+
+cd $WORKSPACE
+for file in `grep -Rl "local=127.0.0.1" *`; do
+  sed -i -e s/local=127.0.0.1/domain=$CASSANDRA_HOST/g -e 's/us-west-2/$AWS_REGION/g' $file;
+done
+for file in `grep -Rl "domain=127.0.0.1" *`; do
+  sed -i -e s/domain=127.0.0.1/domain=$CASSANDRA_HOST/g -e 's/us-west-2/$AWS_REGION/g' $file;
+done
+echo "Setting default cluster to the msl monolith elb"
+for file in `grep -Rl "127.0.0.1" *`; do
+  sed -i -e "s/127.0.0.1/$CASSANDRA_HOST/g" -e 's/us-west-2/$AWS_REGION/g' $file;
+done
+
+cd $WORKSPACE/server/$SERVICE_NAME;
+#mvn compile
+mvn package -P rpm-package
+
+
+echo "Double checking"
+grep -R DEFAULT_CLUSTER *
+find . -type f | grep config$ | xargs cat | grep domain
+find . -type f | grep config$ | xargs cat | grep local
+
+mkdir $WORKSPACE/RPM/
+BUILD_NUMBER=`cat $WORKSPACE/build_number`
+cp $WORKSPACE/server/$SERVICE_NAME/target/rpm/$SERVICE_NAME/RPMS/x86_64/$SERVICE_NAME*.rpm $WORKSPACE/RPM/
--- tools/rpm/postinstall.sh
+++ tools/rpm/postinstall.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo "Environment variables I can use:"
+export
+
 #Using INSTALLING_SERVICE rather than SERVICE_NAME because
 # when this script runs, Jenkins is out of the picture and can't pass in vars
 INSTALLING_SERVICE=EDITME
+echo "Service to install: $INSTALLING_SERVICE"
+INSTALLING_CASSANDRA=EDITCASSANDRA
+echo "Cassandra Host is at: $INSTALLING_CASSANDRA"
 
 if [ $INSTALLING_SERVICE == EDITME ]; then
   echo "Script was supposed to be edited by jenkins job first."
@@ -11,8 +17,31 @@ fi
 
 chmod a+rwx /var/log/msl
 JAR_TO_EXEC=`find /opt/kenzan/$INSTALLING_SERVICE-*-with-dependencies.jar`
+#echo "Found a jar at $JAR_TO_EXEC"
+
+#Commented out because environment config is now moved out of the build phase.
+#Should be safe to delete.
+#cd /tmp/
+#if unzip $JAR_TO_EXEC config.properties; then
+  # #chmod a+rw config.properties
+  # #sed -i -e s/127.0.0.1/$INSTALLING_CASSANDRA/ config.properties
+  # rm -rf config.properties
+  # echo "192.168.4.93 cassandra-msl.kenzan-devops.com" >> /etc/hosts
+  # echo "creating config"
+  # echo "domain=$INSTALLING_CASSANDRA" > config.properties
+  # echo "keyspace=msl" >> config.properties
+  # echo "region=us-east-1" >> config.properties
+  # cat config.properties
+  # echo "adding to zip"
+#  zip -f $JAR_TO_EXEC config.properties
+#else
+#  echo "Editing config failed during jar unpack, reason:"
+#  unzip $JAR_TO_EXEC config.properties#
+#  exit 1
+#fi
 
 if [ `echo $JAR_TO_EXEC | wc -l` == 1 ]; then
+  echo "Found $JAR_TO_EXEC"
   echo "Adding startup line to rc.local" | tee -a /tmp/msl-install.log
   echo "/usr/bin/java8 -jar $JAR_TO_EXEC 2>&1 | tee -a /var/log/msl/$INSTALLING_SERVICE.log" | tee -a /tmp/msl-install.log | tee /opt/kenzan/startup.sh
   chmod +x /opt/kenzan/startup.sh > /dev/null
